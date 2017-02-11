0f988f245b37b379d0b5d75e734e82cd8800c4fc
Mirsait <mavcharenko@gmail.com>
2017-02-12 00:46:31 +0300

    create "Reverse a String"-function

--- Text/reverse_a_string/main.py
+++ Text/reverse_a_string/main.py
@@ -2,3 +2,16 @@
 Reverse a String – Enter a string and the program will reverse it and
 print it out.
  """
+import sys
+
+def reverse_string (text):
+    chars = list(text)
+    chars.reverse()
+    return ''.join(chars)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        text = reverse_string(sys.argv[1])
+    else:
+        text = 'Error: No string!'
+    print(text)
