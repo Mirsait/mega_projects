1518078790f4cfc1e67df25215d249a8866f838f
Mirsait <mavcharenko@gmail.com>
2017-02-18 02:51:34 +0300

    update, it works, no beauty

--- Text/pig_latin/piglatin.py
+++ Text/pig_latin/piglatin.py
@@ -6,21 +6,16 @@ import re
 def get_eng_to_piglatin(text):
     """ translate english to pig latin """
     words = text.replace('\W', ' ').split()
-    #vowels = ['a','e','i','o','u','h']
-    #consonant = ['qu', ]
-    vowels = '\b[aeiouhAEIOUH]\W+'
-    pig_words = words
+    pig_words = ''
     for word in words:
-        stri =''
         # гласные
         res = re.findall(r'\b[aeiouhAEIOUH]\w+', word)
         if res:
-            stri = re.sub(r'\b([aeiouhAEIOUH]\w+)', r'\1ya', word)
+            word = re.sub(r'\b([aeiouhAEIOUH]\w+)', r'\1ya', word)
         else:
-            # согласных
-            stri = re.sub(r'\b([^aeiouhAEIOUH ]+)(\w+)', r'\2\1ay', word)
-        print(stri)
-
+            word = re.sub(r'\b([^aeiouhAEIOUHqQ ]+)(\w+)', r'\2\1ay', word)
+            word = re.sub(r'\b(qu)(\w+)', r'\2\1ay', word)
+        pig_words += word+' '
     return pig_words
 
 
