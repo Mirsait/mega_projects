777fea241dbeca8720b83ebdef859f3b43c8eda9
Mirsait <mavcharenko@gmail.com>
2017-02-18 02:27:31 +0300

    not work "qu"

--- Text/pig_latin/piglatin.py
+++ Text/pig_latin/piglatin.py
@@ -1,11 +1,28 @@
 """
 """
+import sys
+import re
 
 def get_eng_to_piglatin(text):
     """ translate english to pig latin """
+    words = text.replace('\W', ' ').split()
+    #vowels = ['a','e','i','o','u','h']
+    #consonant = ['qu', ]
+    vowels = '\b[aeiouhAEIOUH]\W+'
+    pig_words = words
+    for word in words:
+        stri =''
+        # гласные
+        res = re.findall(r'\b[aeiouhAEIOUH]\w+', word)
+        if res:
+            stri = re.sub(r'\b([aeiouhAEIOUH]\w+)', r'\1ya', word)
+        else:
+            # согласных
+            stri = re.sub(r'\b([^aeiouhAEIOUH ]+)(\w+)', r'\2\1ay', word)
+        print(stri)
 
+    return pig_words
 
-    return piglatin
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
