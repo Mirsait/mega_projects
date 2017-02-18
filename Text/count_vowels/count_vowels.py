cf14b77708d5d66b4c56d8cd3a39b69909c12804
Mirsait <mavcharenko@gmail.com>
2017-02-18 03:47:38 +0300

    Initial count_vowels

---
+++ Text/count_vowels/count_vowels.py
@@ -0,0 +1,18 @@
+'''
+Count Vowels – The program counts the number of
+ vowels in the text and reports a sum of each vowel found.
+'''
+import sys
+import re
+
+def get_count_vowels(text):
+
+    return result
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        text = get_count_vowels(sys.argv[1])
+    else:
+        text = 'Error: No text!'
+    print(text)
---
+++ Text/count_vowels/docs.md
@@ -0,0 +1,3 @@
+#Count Vowels
+
+**The program counts the number of vowels in the text and reports a sum of each vowel found.**
