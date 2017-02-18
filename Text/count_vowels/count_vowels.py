9589e7f4bed83417bfe4115801fed71b52deec52
Mirsait <mavcharenko@gmail.com>
2017-02-18 19:19:22 +0300

    It works

--- Text/count_vowels/count_vowels.py
+++ Text/count_vowels/count_vowels.py
@@ -1,18 +1,16 @@
 '''
-Count Vowels – The program counts the number of
+Count Vowels
+The program counts the number of
  vowels in the text and reports a sum of each vowel found.
 '''
 import sys
 import re
 
 def get_count_vowels(text):
-
-    return result
-
+    return [[char, list(text[:].lower()).count(char)] for char in ('aeiou')]
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         text = get_count_vowels(sys.argv[1])
-    else:
-        text = 'Error: No text!'
-    print(text)
+        for value in text:
+            print(value)
